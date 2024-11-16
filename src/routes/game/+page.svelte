03f04df443507d023e96cad17f13bea5fb6f53b8
JayDos89 <EVERJR1@student.op.ac.nz>
2024-11-17 09:00:20 +1300

    updates

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,24 +1,54 @@
+
 <script>
   import MenuHover from "$lib/MenuHover.svelte";
   import Counter from "$lib/Counter.svelte";
   import TextInput from "$lib/TextInput.svelte";
-  
-  export let title = "Escape the virus sweep";
+  import Touch from "$lib/images/tech_banner.png";
+  import crypto from "$lib/images/crypto.png";
+  import blockchain_machine from "$lib/images/blockchain_machine.png";
+  import keyboard from "$lib/images/Keyboard.png";
+  //export let title = "Escape the virus sweep";
   export let data;
   console.log(data);
-  
+  const code = "WIN";
+  let guess;
+  let tokens = 5;
+  let keyLetter = [];
+  function add(){
+    tokens ++;
+  }
+  function randKey(){
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const index = Math.floor(Math.random() * 26);
+    keyLetter[keyLetter.length + 1] = alphabet[index];
+    tokens -= 1;
+  }
 </script>
 
-<h5>{title}</h5><br>
+<h2>Tokens = {tokens}</h2><br>
+  
+<label for="name"><h3>The word is (WIN) 
+  <input type="text" bind:value={guess} id="guess">           
+</label>
+console.log({guess})
+<div class="container4">
+    <h2>Random Letter Generator</h2>
+    <h4>Cost - 1 Token per letter </h4><br><br><p> Console: {keyLetter}</p>
+    <img src={keyboard} width="300px" alt="of a rainbow keyboard" on:click={randKey} />
+</div>
 
-<!--<section class="container">-->
-    <p>I can see a hiding spot between 13 - 15</p>
-    <p>You need to land there or they will catch you - Don't over step</p><br>
-    <Counter />
-<!--</section>-->
+<section class="container2">
+    <img src={blockchain_machine} width="100%" alt="of blockchain machine" on:click={add}/>
+</section>
+
+<section class="container2">
+  <Counter />
+</section>
+
 
 <section class ="container2">
-    <div class="cryptoFont1">
+    <div>
+      <img src={crypto} width="100%"  alt="pic of human robot hands touching"/> 
       <h1>Crtpo Currency API data</h1> <br>
         Crypto name - {data.btc.name}<br>
         All time high - ${(data.btc.allTimeHighUSD).toFixed(2)} USD<br>
@@ -42,12 +72,43 @@
     font-size: larger;
     font-family: 'Times New Roman', Times, serif;
   }
+
   .container2{
-    max-width: 800px;
+    max-width: 50rem;
     margin: auto;
-    padding: 2px;   
+    padding: 1px;  
+    margin-right: 100px;
+    margin-top: 1px; 
     background: rgb(0, 0, 255); /* White background for content */
+    background-image: none;
     border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 10px #17a82a;
   }
+
+  .container4{
+    max-width: 25rem;
+    margin:auto;
+    padding: 1px;  
+    margin-right: 100px;
+    margin-top: 20px;    
+  }
+
+  .Button2{
+    background:    #17a82a;
+    background:    linear-gradient(#17a82a, #073763);
+    border-radius: 50px;
+    padding:       20px 45px;
+    color:         #ffffff;
+    display:       inline-block;
+    font:          normal bold 26px/1 "Roboto", sans-serif;
+    text-align:    center;
+  }
+
+  .flex2{
+    display: flex;
+    align-items: start;
+    justify-content:flex-start;
+    max-width: 500px;
+}
+
 </style>
--- src/routes/technologys/+page.svelte
+++ src/routes/technologys/+page.svelte
@@ -11,28 +11,37 @@
 
 <section class="Container">
     <figure>
-        <img src={Techbanner} alt="crypto pic"/>
-        <figcaption> <h1>Cryptocurrencys</h1>
-            <p>The birth of the first cryptocurrency
+        <img src={Techbanner} alt="of a crypto banner" width="750px"/>
+        <figcaption> <h1>Cryptocurrencys</h1><br>
+            <h2>What is bitcoin?</h2><p>The birth of the first cryptocurrency
                 Bitcoin was first released by Satoshi Nakamoto on 3rd Jan 2009 as a new form of money. It is a novel invention that has given birth to the technology we call blockchain today. Little did people know that it will have an impact wider than just reinventing the antiquated financial system. It has in fact, gone on to inspire a whole generation of developers</p>
             <p>There are thousands of cryptocurrencies. Some of the best known include:
                 Bitcoin:
-                Founded in 2009, Bitcoin was the first cryptocurrency and is still the most commonly traded. The currency was developed by Satoshi Nakamoto – widely believed to be a pseudonym for an individual or group of people whose precise identity remains unknown.</p>
-            </figcaption>           
+                Founded in 2009, Bitcoin was the first cryptocurrency and is still the most commonly traded. The currency was developed by Satoshi Nakamoto – widely believed to be a pseudonym for an individual or group of people whose precise identity remains unknown.</p></figcaption>
     </figure>
 </section>
-    
+<section class="Container"> 
+    <figure>
+        <img src={Nueralink} alt="of Nueralink" width="400px" />
+        <figcaption><h4>Neuralink's first human patient able to control mouse through thinking, Musk says
+            By Reuters
+            February 21, 20248:22 AM GMT+13Updated 9 months ago</h4></figcaption>
+    </figure>
+</section> 
+<div class="Container">    
+    <img src={Hands2} alt="of robot and human hands"/>   
+</div>
+
 <div class="Container">
-    <img src={Nueralink} alt="pic of Nueralink"/> 
-    <img src={Hands2} alt="pic of robot and human hands"/>
-    <img src={AppleVision} alt="pic of Apple Vision Pro"/>
+    <img src={AppleVision} alt="of Apple Vision Pro"/>
 </div>
 
 <style>
    .Container{
     display: flex;
     justify-content: space-between;
-    gap: 20px;
+    gap: 2px;
     padding: 1px;
+    
    } 
 </style>
